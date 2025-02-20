d4680a5cb71ffdaa51b6ae811f7a9738a143d55c
shibuya takumi <takm.reason@gmail.com>
2025-02-21 00:42:46 +0900

    test: インフラストラクチャテストの包括的な改善
    - VPC設定の検証を実際のCloudFormationテンプレートに合わせて修正
    - RDSインスタンスの設定テストを型安全に改善
    - S3バケットのライフサイクルルール検証を詳細化
    - ECSクラスターとFargateサービスのテストを実装状態に即して更新
    - オートスケーリング設定の検証を追加
    - セキュリティグループの検証方法を改善
    - 各コンポーネントの出力検証を追加
    
    すべてのテストが正常に通過することを確認済み

--- test/my-cdk-project.test.ts
+++ test/my-cdk-project.test.ts
@@ -1,18 +1,126 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
-import * as MyCdkProject from '../lib/my-cdk-project-stack';
+import { Template, Match } from 'aws-cdk-lib/assertions';
+import { SmallScaleStack } from '../lib/small-scale-stack';
 
-describe('MyCdkProject Stack', () => {
+describe('SmallScaleStack', () => {
     const app = new cdk.App();
-    const stack = new MyCdkProject.MyCdkProjectStack(app, 'MyTestStack');
+    const stack = new SmallScaleStack(app, 'TestSmallScaleStack');
     const template = Template.fromStack(stack);
 
-    test('Stack has been created', () => {
-        // テンプレートが作成されていることを確認
-        template.templateMatches({
-            "Resources": {}
+    // VPCのテスト
+    test('VPC should be created with correct configuration', () => {
+        template.hasResourceProperties('AWS::EC2::VPC', {
+            EnableDnsHostnames: true,
+            EnableDnsSupport: true,
+            CidrBlock: '10.0.0.0/16',
+        });
+
+        // サブネットとNAT Gatewayの数を確認
+        template.resourceCountIs('AWS::EC2::Subnet', 4); // 2 AZs × (Public + Private)
+        template.resourceCountIs('AWS::EC2::NatGateway', 1);
+    });
+
+    // RDSのテスト
+    test('RDS instance should be created with correct configuration', () => {
+        template.hasResourceProperties('AWS::RDS::DBInstance', {
+            Engine: 'postgres',
+            EngineVersion: '15',
+            DBInstanceClass: 'db.t4g.small',
+            MultiAZ: false,
+            AllocatedStorage: Match.stringLikeRegexp('20'),
+            MaxAllocatedStorage: 100,
+            BackupRetentionPeriod: 7,
+            DeleteAutomatedBackups: true,
         });
     });
 
-    // ここに追加のテストケースを記述できます
+    // S3バケットのテスト
+    test('S3 bucket should be created with correct configuration', () => {
+        template.hasResourceProperties('AWS::S3::Bucket', {
+            VersioningConfiguration: {
+                Status: 'Enabled'
+            },
+            BucketEncryption: {
+                ServerSideEncryptionConfiguration: [
+                    {
+                        ServerSideEncryptionByDefault: {
+                            SSEAlgorithm: 'AES256'
+                        }
+                    }
+                ]
+            },
+            LifecycleConfiguration: {
+                Rules: [
+                    Match.objectLike({
+                        ExpirationInDays: 365,
+                        NoncurrentVersionExpiration: {
+                            NoncurrentDays: 30
+                        },
+                        Status: 'Enabled'
+                    })
+                ]
+            }
+        });
+    });
+
+    // ECSクラスターとFargateサービスのテスト
+    test('ECS Cluster and Fargate Service should be created with correct configuration', () => {
+        // ECSクラスター
+        template.hasResourceProperties('AWS::ECS::Cluster', {});
+
+        // Fargateサービス
+        template.hasResourceProperties('AWS::ECS::Service', {
+            LaunchType: 'FARGATE',
+            DesiredCount: 1,
+            NetworkConfiguration: {
+                AwsvpcConfiguration: {
+                    AssignPublicIp: 'DISABLED'
+                }
+            }
+        });
+
+        // タスク定義
+        template.hasResourceProperties('AWS::ECS::TaskDefinition', {
+            Cpu: '512',
+            Memory: '1024',
+            NetworkMode: 'awsvpc',
+            RequiresCompatibilities: ['FARGATE']
+        });
+    });
+
+    // Auto Scalingのテスト
+    test('Auto Scaling should be configured correctly', () => {
+        template.hasResourceProperties('AWS::ApplicationAutoScaling::ScalingPolicy', {
+            PolicyType: 'TargetTrackingScaling',
+            TargetTrackingScalingPolicyConfiguration: {
+                TargetValue: 75,
+                ScaleInCooldown: 300,
+                ScaleOutCooldown: 300,
+                PredefinedMetricSpecification: {
+                    PredefinedMetricType: 'ECSServiceAverageCPUUtilization'
+                }
+            }
+        });
+
+        template.hasResourceProperties('AWS::ApplicationAutoScaling::ScalableTarget', {
+            MinCapacity: 1,
+            MaxCapacity: 2,
+        });
+    });
+
+    // セキュリティグループのテスト
+    test('Security Groups should be configured correctly', () => {
+        // RDSセキュリティグループの存在確認
+        template.hasResourceProperties('AWS::EC2::SecurityGroupIngress', Match.objectLike({
+            FromPort: 5432,
+            IpProtocol: 'tcp',
+            ToPort: 5432,
+        }));
+    });
+
+    // 出力のテスト
+    test('Stack outputs should be defined', () => {
+        template.hasOutput('LoadBalancerDNS', {});
+        template.hasOutput('S3BucketName', {});
+    });
 });
